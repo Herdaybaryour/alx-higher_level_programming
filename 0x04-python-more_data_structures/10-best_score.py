2113323e4302c359b3a2d742016728f95b449729
Herdaybaryour <bayo450@gmail.com>
2022-12-10 06:07:55 -0800

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,14 @@
 #!/bin/python3
 
-def best_score(my_dict):
+def best_score(a_dict):
 
-    if my_dict is None or my_dict == {}:
+    if a_dict is None or a_dict == {}:
 
         return None
 
-    biggest = max(my_dict.values())
+    biggest = max(a_dict.values())
 
-    for key, value in my_dict.items():
+    for key, value in a_dict.items():
 
         if value is biggest:
 
