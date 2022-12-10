63a661e4c584e95db494e30d54064cbe1c11bf30
Herdaybaryour <bayo450@gmail.com>
2022-12-10 06:46:29 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-
-  # define a dictionary with the values of each Roman numeral
 
   numeral_values = {
 
@@ -21,42 +20,18 @@ def roman_to_int(roman_string):
   }
 
 
-
-  # initialize the result to 0
-
   result = 0
-
-
-
-  # iterate over the Roman numeral string in reverse
 
   for i in range(len(roman_string)-1, -1, -1):
 
-    # get the current numeral and its value
-
     numeral = roman_string[i]
-
     value = numeral_values[numeral]
-
-
-
-    # if the numeral is the first or the last in the string
-
-    # or if the numeral to the right has a lower value,
-
-    # add the value to the result
 
     if i == 0 or i == len(roman_string)-1 or numeral_values[roman_string[i-1]] <= value:
 
       result += value
 
-    # otherwise, subtract the value from the result
-
     else:
-
       result -= value
 
-
-
   return result
-
